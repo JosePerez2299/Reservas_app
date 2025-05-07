9ac7c0a64add4dd5b3939f7f4ddb72b08522bbec
Jose Perez <16-10882@usb.ve>
2025-05-07 11:39:49 -0400

    Actualizar configuración de idioma a español, mejorar formularios de registro y crear vistas de inicio y registro.

--- reservas/forms.py
+++ reservas/forms.py
@@ -1,20 +1,43 @@
-# users/forms.py
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class TailwindFormMixin:
-    tailwind_classes = 'block w-full p-2 border rounded focus:outline-none focus:ring'
+class UserCreationForm(UserCreationForm):
+    error_css_class = 'error'
+    required_css_class = 'required'
+    label_suffix = ':'
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in self.fields.values():
-            existing = field.widget.attrs.get('class', '')
-            field.widget.attrs['class'] = f'{existing} {self.tailwind_classes}'
-
-class SignUpForm(TailwindFormMixin, UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
+        labels = {
+            'username': 'Nombre de usuario',
+            'email': 'Correo electrónico',
+            'password1': 'Contraseña',
+            'password2': 'Confirmar contraseña',
+        }
+        error_messages = {
+            'username': {
+                'required': 'Este campo es obligatorio.',
+                'unique': 'Este nombre de usuario ya está en uso.',
+            },
+            'email': {
+                'required': 'Este campo es obligatorio.',
+                'invalid': 'Introduce una dirección de correo electrónico válida.',
+            },
+            'password1': {
+                'required': 'Este campo es obligatorio.',
+            },
+            'password2': {
+                'required': 'Este campo es obligatorio.',
+            },
+        }
+
--- reservas/views.py
+++ reservas/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.urls import reverse_lazy
 from django.views import View
+from django.contrib.auth.models import Group
+from django.views.generic import TemplateView, CreateView
 from django.template import loader
-from reservas.forms import SignUpForm
+from reservas.forms import UserCreationForm
 
-class HomeView(View):
-    def get(self, request):
-        template = loader.get_template('index.html')
-        return HttpResponse(template.render())
+class HomeView(TemplateView):
+    template_name = 'home.html'
     
 
 class LoginView(View):
@@ -20,15 +21,16 @@ class LoginView(View):
         return HttpResponse("This is the login view (POST).")
 
 
-class SignupView(View):
-    def get(self, request):
-        form = SignUpForm()
-        return render(request, 'signup.html', {'form': form})
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy('')  # Cambia esto a donde quieras redirigir al usuario
 
-    def post(self, request):
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('/')   # o la URL que quieras
-        return render(request, 'signup.html', {'form': form})
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        # Iniciar sesión automáticamente
+        login(self.request, self.object)
+        # Añadir al grupo 'usuario'
+        # grupo = Group.objects.get(name='usuario')
+        # self.object.groups.add(grupo)
+        return response
