33b00f0fa75f095b34136effa575ac192400b90e
Jose Perez <16-10882@usb.ve>
2025-05-14 11:39:46 -0400

    primer setteo del crud para los usuarios. breve descripcion de los casos de uso (borrador)

---
+++ casosdeuso.md
@@ -0,0 +1,34 @@
+- Como admin:
+    - puedo crear, listar, ver, eliminar espacios 
+        - SIN RESTRICCION
+    - Para los espacios, asignar moderadores
+    - Ver lista de reserva por espacio (Todas las reservas)
+    - puedo crear, listar, eliminar usuarios
+        - Restriccion: 
+            - el usuario a crear debe ser moderador o usuario normal, no administrador
+    - eliminar reservas, ver
+        - Sin restriccion
+
+    - No puedo crear reservas
+
+
+- Como moderador:
+    - ver los espacios gestionados por mi usuario
+        - Ver reseras por espacios (un espacio en especifico)
+    - ver lista de reservas (todas las reservas de los espacios gestionados )
+    - aprobar, rechazar reservas (de las salas ):
+        - Solo aprobar reservas si yo estoy autorizado a moderar ese espacio
+    - puedo crear, listar, eliminar? de USUARIOS:
+        - Restriccion:
+            - solo usuarios normales. (Quizas ser una eliminacion pasiva, no eliminar por completo)
+
+
+- Como usuario:
+    - crear, gestionar reservas:
+        - eliminar, ver estado ( solo de mis reservas)
+
+
+- El sistema:
+    -  Manejar reservas vencidas (si la fecha actual es mayor a la fecha de la reserva, cambiar el estatus)
+    - Panel estadisticas??
+
--- reservas/library/views/usuarios.py
+++ reservas/library/views/usuarios.py
@@ -1,27 +1,60 @@
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
-from reservas.library.forms.auth import User, UserCreationForm
-from reservas.models import Espacio
-from django.contrib.messages.views import SuccessMessageMixin
-from django.contrib import messages
-# TODO: CRUD
-from django.db.models import Q
-from django.views.generic import ListView
-from django.db.models import Q
-from django.views.generic import ListView
 
-class UserListView(ListView):
+from django.contrib import messages
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+from reservas.library.forms.auth import User
+from django.contrib.messages.views import SuccessMessageMixin
+from reservas.library.mixins.list_helpers import AutoFilterMixin, ColumnsMixin
+
+class UsuarioListView(PermissionRequiredMixin, AutoFilterMixin, ColumnsMixin, ListView):
     model = User
-    template_name = 'espacios/list.html'
-    context_object_name = 'usuarios'
+    permission_required = 'auth.view_user'
+    template_name = 'table_view.html'
     paginate_by = 10
 
+    list_display = [
+        ('username', 'Usuario'),       
+        ('first_name', 'Nombre'),        
+    ]
 
-class UsuarioCreate(CreateView):
-    form_class = UserCreationForm
-    template_name = 'espacios/create.html'
-    success_url = reverse_lazy('')  
+    def get_queryset(self):
+        return User.objects.filter(groups__name__in=['moderador', 'usuario']).distinct()
 
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        return response
+
+# TO DO todo lo de abajo :D
+class UsuarioCreateView(SuccessMessageMixin, CreateView):
+    model = User
+    template_name = 'create.html'
+    fields = ['nombre', 'ubicacion', 'capacidad', 'tipo']
+    success_url = reverse_lazy('usuario')
+    success_message = "¡El espacio fue creado con éxito!"
+
+    def form_invalid(self, form):
+        messages.error(self.request, "Hubo un error al crear el espacio. Por favor, verifica los datos.")
+        return super().form_invalid(form)
+
+
+class UsuarioUpdateView(UpdateView):
+    model = User
+    fields = ['nombre', 'ubicacion', 'capacidad', 'tipo', 'disponible']
+    template_name = 'update.html'  # plantilla parcial
+    context_object_name = 'espacio'
+
+    def get_success_url(self):
+        return reverse_lazy('usuario')
+    
+class UsuarioDeleteView(DeleteView):
+    model = User
+    template_name = 'delete.html'
+    context_object_name = 'espacio'
+    success_url = reverse_lazy('usuario')
+
+    def delete(self, request, *args, **kwargs):
+        """
+        Override para añadir mensaje antes de redirigir.
+        """
+        obj = self.get_object()
+        messages.success(request, f"El espacio “{obj.nombre}” fue eliminado con éxito.")
+        return super().delete(request, *args, **kwargs)
