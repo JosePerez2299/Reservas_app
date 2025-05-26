e38e076dc1b06916d8614c8827d7bfa08948434c
Jose Perez <16-10882@usb.ve>
2025-05-26 13:20:10 -0400

    cambios minimos

--- reservas/library/views/espacios.py
+++ reservas/library/views/espacios.py
@@ -104,7 +104,7 @@ class EspacioDeleteView(DeleteView):
     """
     model = Espacio
     template_name = 'reservas/delete.html'
-    success_url = reverse_lazy('espacio')  # o reverse_lazy(...) si prefieres
+    success_url = reverse_lazy('espacio') 
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
