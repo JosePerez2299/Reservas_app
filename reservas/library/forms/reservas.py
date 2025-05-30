2e6f4e97a796736e6e9689fefa0e006ff9844002
Jose Perez <16-10882@usb.ve>
2025-05-30 14:50:41 -0400

    - Reestructuracion de la tabla de models. - El formulario de creacion de reservas corregido algunos bugs

--- reservas/library/forms/reservas.py
+++ reservas/library/forms/reservas.py
@@ -4,6 +4,7 @@ from reservas.models import Reserva, Espacio, Usuario
 from datetime import date, timedelta
 from django_select2.forms import Select2Widget
 
+
 class UsuarioWidget(Select2Widget):
     def build_attrs(self, *args, **kwargs):
         attrs = super().build_attrs(*args, **kwargs)
@@ -21,14 +22,18 @@ class UsuarioWidget(Select2Widget):
         super().__init__(*args, **kwargs)
         self.choices = []
         self.attrs = self.build_attrs(self.attrs)
+
+
 class ReservaCreateForm(forms.ModelForm):
     class Meta:
         model = Reserva
-        fields = ['usuario', 'fecha_uso', 'hora_inicio', 'hora_fin', 'espacio', 'motivo', 'estado']
+        fields = ['usuario', 'fecha_uso', 'hora_inicio',
+                  'hora_fin', 'espacio', 'motivo']
         widgets = {
             'fecha_uso': forms.DateInput(
                 format='%Y-%m-%d',
-                attrs={'type': 'date', 'min': date.today().isoformat(), 'max': (date.today() + timedelta(days=90)).isoformat()}
+                attrs={'type': 'date', 'min': date.today().isoformat(), 'max': (
+                    date.today() + timedelta(days=90)).isoformat()}
             ),
             'hora_inicio': forms.TimeInput(attrs={'type': 'time'}),
             'hora_fin': forms.TimeInput(attrs={'type': 'time'}),
@@ -41,52 +46,49 @@ class ReservaCreateForm(forms.ModelForm):
 
         # Si es administrador, mostrar selector de usuarios
         if self.request.user.is_admin:
-            self.fields['usuario'].queryset = Usuario.objects.filter(Q(groups__name='usuario') | Q(groups__name='moderador') | Q(id=self.request.user.id))
-        
+            self.fields['usuario'].queryset = Usuario.objects.filter(
+                Q(groups__name='usuario') | Q(groups__name='moderador'))
+
         # Si es moderador, mostrar selector de usuarios
         elif self.request.user.is_moderador:
             self.fields['usuario'].queryset = Usuario.objects.filter(
-                Q(groups__name='usuario') & 
-                Q(ubicacion=self.request.user.ubicacion) & 
-                Q(piso=self.request.user.piso) | Q(id=self.request.user.id)
+                (
+                    Q(groups__name='usuario') &
+                    Q(ubicacion=self.request.user.ubicacion) &
+                    Q(piso=self.request.user.piso)
+                ) | Q(id=self.request.user.id)
             )
 
-        
         # Si es usuario, mostrar selector de su usuario
         elif self.request.user.is_usuario:
-            self.fields['usuario'].queryset = Usuario.objects.filter(pk=self.request.user.pk)
             self.fields['usuario'].initial = self.request.user
             self.fields['usuario'].disabled = True
             self.fields['usuario'].help_text = "No puedes cambiar este campo"
-            self.fields['estado'].widget = forms.HiddenInput()
-            self.fields['estado'].disabled = True
-            self.fields['estado'].initial = 'pendiente'
 
- 
+
 
 class ReservaUpdateForm(ReservaCreateForm):
     estado = forms.ChoiceField(choices=Reserva.ESTADO_CHOICES)
 
     class Meta(ReservaCreateForm.Meta):
         fields = ReservaCreateForm.Meta.fields + ['estado']
-    
+
     def __init__(self, request, *args, **kwargs):
         super().__init__(request, *args, **kwargs)
+        self.fields['usuario'].disabled = True
         self.fields['espacio'].disabled = True
-        
-        # Garantiza que, además de lo que ya filtraste,
-        # también esté el usuario que figura en la reserva:
-        if self.instance and self.instance.pk:
-            qs = self.fields['usuario'].queryset
-            self.fields['usuario'].queryset = (
-                qs | Usuario.objects.filter(pk=self.instance.usuario.pk)
-            )
 
-        if self.request.user.is_usuario:
+
+        if self.request.user.is_admin:
+            return
+
+        elif self.request.user.is_moderador:
+            self.fields['estado'].disabled = self.instance.espacio.ubicacion != self.request.user.ubicacion or self.instance.espacio.piso != self.request.user.piso
+            
+
+        elif self.request.user.is_usuario:
             self.fields['estado'].widget = forms.HiddenInput()
             self.fields['estado'].disabled = True
-
-
 
     def save(self, commit=True):
         # Primero, instancia sin guardar aún
@@ -97,5 +99,3 @@ class ReservaUpdateForm(ReservaCreateForm):
         if commit:
             instance.save()
         return instance
-
-   
--- reservas/templatetags/table_actions.py
+++ reservas/templatetags/table_actions.py
@@ -2,21 +2,37 @@
 from django import template
 register = template.Library()
 
+
+# Esta etiqueta de template solo se utiliza para renderizar los botones
+# para editar o eliminar un objeto. La logica de permisos se maneja en
+# el backend, por lo que no se verifica permisos en este punto.
+# La razón de no verificar permisos en este punto es que se quieren
+# mostrar los botones de editar y eliminar en la tabla de objetos,
+# pero no se quieren habilitar para usuarios que no tengan permisos.
+# En su lugar, se dejan deshabilitados, y se verifica el permiso en
+# el view de edicion o eliminacion.
 @register.simple_tag
-def puede_editar(objeto, user):
-    return (
-        (  user.is_admin
-            or
-            user.is_moderador)
-        and objeto.estado == 'pendiente'
-    )
+def puede_editar(objeto, user, model):
+
+    if model == 'reserva':
+        return objeto.estado == 'pendiente'
+
+    elif model == 'usuario' or model == 'Usuario':
+        return True
+
+
+    elif model == 'espacio':
+        return True
+        
+    else:
+        return False
 
 @register.simple_tag
-def puede_eliminar(objeto, user):
-    return (
-        (  user.is_admin
-            or
-            user.is_moderador)
-        and objeto.estado == 'pendiente'
-    )
-    
+def puede_eliminar(objeto, user, model):
+    if model == 'reserva':
+        return objeto.estado == 'pendiente'
+    elif model == 'usuario':
+        return user.is_admin
+    elif model== 'espacio':
+        return True
+        
