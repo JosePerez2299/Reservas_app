b234a8224687ee9b149f421dcd7c0d41c7427c76
Jose Perez <16-10882@usb.ve>
2025-06-05 11:26:29 -0400

    Estadisticas mensuales y cards para usuarios

--- reservas/library/utils/utils.py
+++ reservas/library/utils/utils.py
@@ -88,7 +88,43 @@ def get_stats_administrador(request):
     
     
 def get_stats_usuario(request):
-    return {'cards': [], 'month_summary': []}
+    month = datetime.now().month
+    year = datetime.now().year
+    reservas_pendientes = Reserva.objects.filter(estado='pendiente', usuario=request.user).count()
+    reservas_aprobadas = Reserva.objects.filter(estado='aprobada', usuario=request.user).count()
+    reservas_rechazadas = Reserva.objects.filter(estado='rechazada', usuario=request.user).count()
+
+    reservas_mes = Reserva.objects.filter(usuario=request.user, fecha_uso__month=month, fecha_uso__year=year).count()
+    reservas_aprobadas_mes = Reserva.objects.filter(estado='aprobada', usuario=request.user, fecha_uso__month=month, fecha_uso__year=year).count()
+    reservas_pendientes_mes = Reserva.objects.filter(estado='pendiente', usuario=request.user, fecha_uso__month=month, fecha_uso__year=year).count()
+    reservas_rechazadas_mes = Reserva.objects.filter(estado='rechazada', usuario=request.user, fecha_uso__month=month, fecha_uso__year=year).count()
+
+
+    reservas_aprobadas_percent = (reservas_aprobadas_mes / reservas_mes) * 100
+    reservas_pendientes_percent = (reservas_pendientes_mes / reservas_mes) * 100
+    reservas_rechazadas_percent = (reservas_rechazadas_mes / reservas_mes) * 100
+
+    cards = [
+        {'title': 'Mis Reservas Aprobadas', 'value': reservas_aprobadas, 'icon': 'success', 'color': 'text-success'},
+        {'title': 'Mis Reservas Pendientes', 'value': reservas_pendientes, 'icon': 'warning', 'color': 'text-warning'},
+        {'title': 'Mis Reservas Rechazadas', 'value': reservas_rechazadas, 'icon': 'error', 'color': 'text-error'},
+    ]
+
+    month_summary = {
+        'total': reservas_mes,
+        'items': [
+            {'title': 'Reservas Aprobadas', 'value': floor(reservas_aprobadas_percent), 'count': reservas_aprobadas_mes, 'color': 'success'},
+            {'title': 'Reservas Pendientes', 'value': floor(reservas_pendientes_percent), 'count': reservas_pendientes_mes, 'color': 'warning'},
+            {'title': 'Reservas Rechazadas', 'value': floor(reservas_rechazadas_percent), 'count': reservas_rechazadas_mes, 'color': 'error'},
+        ]
+    }
+
+    reservas_stats = {
+        'cards': cards,
+        'month_summary': month_summary,
+    }
+
+    return reservas_stats   
 
 
 def get_stats_moderador(request):
