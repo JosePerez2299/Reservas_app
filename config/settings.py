d3fb9152d9b38f476fd94bd81b0ce5ceba52ce87
Jose Perez <16-10882@usb.ve>
2025-08-29 13:34:17 -0400

    chore: actualizar .gitignore y modificar configuración de base de datos
    - Se ha actualizado el archivo .gitignore para ignorar todos los archivos .sqlite3.
    - Se ha cambiado el nombre de la base de datos en la configuración de Django a 'db.ver2.sqlite3'.
    - Se han eliminado clases CSS innecesarias y se han añadido nuevas para mejorar la gestión de estilos en el archivo output.css.

--- config/settings.py
+++ config/settings.py
@@ -114,7 +114,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.ver2.sqlite3',
     }
 }
 
