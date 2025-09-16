33379fe6a0b080df0a4e8daf3055b0a7e90319b6
Jose Perez <16-10882@usb.ve>
2025-09-16 14:07:21 -0400

    Signal para detectar el cambio de disponibilidad de un espacio

--- apps/espacios/apps.py
+++ apps/espacios/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class EspaciosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.espacios'
+    def ready(self):
+        import apps.espacios.signals 
